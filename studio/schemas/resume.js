bbde9f6f7ecb4757efe9e19042d4c618b887e923
Allan Lasser <lasser.allan@gmail.com>
2022-03-27 19:19:04 -0400

    Adds Resume to studio

---
+++ studio/schemas/resume.js
@@ -0,0 +1,26 @@
+export const position = {
+  title: "Position",
+  name: "position",
+  type: "object",
+  fields: [
+    { name: "role", type: "string", title: "Role" },
+    { name: "company", type: "string", title: "Company" },
+    { name: "description", type: "text", title: "Description" },
+  ],
+};
+
+const resume = {
+  name: "resume",
+  title: "Resume",
+  type: "document",
+  __experimental_actions: ["create", "update", /*'delete',*/ "publish"],
+  fields: [
+    {
+      type: "array",
+      name: "history",
+      of: [{ type: "position" }],
+    },
+  ],
+};
+
+export default resume;
