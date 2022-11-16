280a9ec8ee79d34cd1dae85e893753dbfd7dff9c
Allan Lasser <lasser.allan@gmail.com>
2022-11-16 15:38:09 -0500

    Updates desk structure and icons

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,4 +1,12 @@
 // /deskStructure.js
+import {
+  Library,
+  Bookmark,
+  Pencil,
+  Briefcase,
+  LayoutGrid,
+  Scroll,
+} from "lucide-react";
 import S from "@sanity/desk-tool/structure-builder";
 
 const structure = () =>
@@ -6,7 +14,25 @@ const structure = () =>
     .title("Content")
     .items([
       S.listItem()
+        .title("Posts")
+        .icon(Pencil)
+        .child(S.documentList().title("Posts").filter('_type == "post"')),
+      S.listItem()
+        .title("Notes")
+        .icon(Bookmark)
+        .child(S.documentList().title("Notes").filter('_type == "note"')),
+      S.listItem()
+        .title("Library")
+        .icon(Library)
+        .child(S.documentList().title("Sources").filter('_type == "source"')),
+      S.divider(),
+      S.listItem()
+        .title("Projects")
+        .icon(Briefcase)
+        .child(S.documentList().title("Projects").filter('_type == "project"')),
+      S.listItem()
         .title("Portfolio")
+        .icon(LayoutGrid)
         .child(
           S.document()
             .schemaType("portfolio")
@@ -15,11 +41,21 @@ const structure = () =>
         ),
       S.listItem()
         .title("Resume")
+        .icon(Scroll)
         .child(
           S.document().schemaType("resume").documentId("resume").title("Resume")
         ),
+      S.divider(),
       ...S.documentTypeListItems().filter(
-        (listItem) => !["resume", "portfolio"].includes(listItem.getId())
+        (listItem) =>
+          ![
+            "resume",
+            "portfolio",
+            "project",
+            "post",
+            "note",
+            "source",
+          ].includes(listItem.getId())
       ),
     ]);
 
