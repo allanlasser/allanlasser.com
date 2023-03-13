eedb58ea9f8b8a018c3dd82fbaee64740cc636df
Allan Lasser <lasser.allan@gmail.com>
2023-03-13 11:29:27 -0400

    More resilient data fetching from OpenLibrary

--- src/data/getBookData.ts
+++ src/data/getBookData.ts
@@ -7,7 +7,12 @@ export interface BookData {
 /** Given an ISBN, get additional book data from the OpenLibrary API */
 export default async function getBookData(isbn: string): Promise<BookData> {
   const res = await fetch(`https://openlibrary.org/isbn/${isbn}.json`);
-  const json = res.ok ? await res.json() : {};
+  let json;
+  try {
+    json = res.status === 200 ? await res.json() : {};
+  } catch (e) {
+    json = {};
+  }
   return {
     publishers: json.publishers,
     publishedOn: json.publish_date,
