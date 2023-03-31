2c5d5b10cd7dcc59e69942a2883b11a3a358426a
Allan Lasser <lasser.allan@gmail.com>
2023-03-31 12:19:22 -0400

    Refactor status into component module

--- src/components/status/status.tsx
+++ src/components/status/status.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import getStatus from "src/data/getStatus";
+import styles from "./status.module.css";
 
 export const revalidate = 60;
 
@@ -7,5 +8,5 @@ export default async function Status({
   children,
 }: React.PropsWithChildren<{}>) {
   const status = await getStatus();
-  return <p>{status}</p>;
+  return <p className={styles.status}>{status}</p>;
 }
