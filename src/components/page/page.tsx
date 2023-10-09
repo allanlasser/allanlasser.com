1014ff158755353e07387d6b5e88db18e66656b6
Allan Lasser <lasser.allan@gmail.com>
2023-10-09 17:14:33 -0400

    Adds Search to page header

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -4,6 +4,7 @@ import cx from "classnames";
 import typography from "src/styles/typography.module.css";
 import styles from "./page.module.css";
 import Navigation from "../navigation";
+import Search from "../search";
 
 export interface PageProps {
   htmlTitle?: string;
@@ -18,6 +19,7 @@ export default function Page(props: React.PropsWithChildren<PageProps>) {
       <header className={cx(styles.container)}>
         {isDev && <div className={styles.envBanner}>Dev Mode</div>}
         <Navigation />
+        <Search className={styles.searchBar} />
       </header>
       <main className={cx(styles.container)}>
         {title && <h1 className={styles.title}>{title}</h1>}
