0361795a043183be11ca9f6612ae6cee2de43c25
Allan Lasser <lasser.allan@gmail.com>
2023-10-10 21:56:07 -0400

    Fix for preview URL base

--- src/utils/getSiteUrl.ts
+++ src/utils/getSiteUrl.ts
@@ -1,7 +1,8 @@
 export default function getSiteUrl() {
   let protocol = "https";
   let domain = process.env.SITE_URL ?? process.env.NEXT_PUBLIC_SITE_URL;
-  switch (process.env.VERCEL_ENV) {
+  let env = process.env.VERCEL_ENV ?? process.env.NEXT_PUBLIC_VERCEL_ENV;
+  switch (env) {
     case "preview":
       domain = process.env.VERCEL_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
       break;
