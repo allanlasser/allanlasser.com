3690337fd3fcb37deb95f80f44461ff78b856953
Allan Lasser <lasser.allan@gmail.com>
2023-10-20 14:10:16 -0400

    Fixes styling of images in body content

--- src/components/image-block/image-block.tsx
+++ src/components/image-block/image-block.tsx
@@ -1,7 +1,6 @@
-
-import cx from 'classnames';
+import cx from "classnames";
 import Image from "next/image";
-import styles from './image-block.module.css';
+import styles from "./image-block.module.css";
 
 export interface ImageBlockProps {
   src: string;
@@ -16,32 +15,69 @@ export interface ImageBlockProps {
   alignment?: "left" | "right" | "center";
 }
 
-function SourceLink({source, children}: React.PropsWithChildren<{source?: string}>) {
-  if (source) {
-    return <a className={styles.sourceLink} href={source} rel="noreferrer" target="_blank">{children}</a>
+function SourceLink({
+  href,
+  children,
+}: React.PropsWithChildren<{ href?: string }>) {
+  if (href) {
+    return (
+      <a
+        className={styles.sourceLink}
+        href={href}
+        rel='noreferrer'
+        target='_blank'
+      >
+        {children}
+      </a>
+    );
   }
-  return <>children</>;
+  return <span className={styles.sourceLink}>{children}</span>;
+}
+
+function ImageBlockCaption({
+  title,
+  author,
+  credit,
+  caption,
+  source,
+}: Partial<ImageBlockProps>) {
+  if (!title && !author && !credit && !caption && !source) return null;
+  return (
+    <figcaption className={styles.imageBlockCaption}>
+      {title && <p className={styles.title}>{title}</p>}
+      {caption && <p className={styles.caption}>{caption}</p>}
+      {(credit || author) && (
+        <SourceLink href={source}>{credit ?? author ?? "Source"}</SourceLink>
+      )}
+    </figcaption>
+  );
 }
 
 export default function ImageBlock(props: ImageBlockProps) {
-  const {src, width, height, alt, title, author, credit, caption, source, alignment} = props;
+  const {
+    src,
+    width,
+    height,
+    alt,
+    title,
+    author,
+    credit,
+    caption,
+    source,
+    alignment,
+  } = props;
   return (
-    <figure className={cx(styles.imageBlock, styles[alignment ?? 'left'])}>
-      <div className={styles.imageBlockImage}><Image
-        alt={alt ?? ''}
+    <figure className={cx(styles.imageBlock, styles[alignment ?? "center"])}>
+      <Image
+        className={styles.imageBlockImage}
+        alt={alt ?? ""}
         title={title}
         width={width}
         height={height}
         src={src ?? ""}
         sizes='100vw'
-      /></div>
-      {(title || caption || author) && (
-        <figcaption className={styles.imageBlockCaption}>
-          {title && <div className={styles.title}>{title}</div>}
-          {author && <div className={styles.author}>{author}</div>}
-          {credit && <SourceLink source={source}><cite className={styles.imageBlockCredit}>{credit}</cite></SourceLink>}
-        </figcaption>
-      )}
+      />
+      <ImageBlockCaption {...props} />
     </figure>
   );
 }
