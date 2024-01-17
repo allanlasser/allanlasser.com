2ffa969c12491b009a9ee61885720b568cde7bec
Allan Lasser <lasser.allan@gmail.com>
2024-01-16 19:19:24 -0500

    Updates headers on ATOM and JSON feed routes

--- src/app/feeds/atom.xml/route.ts
+++ src/app/feeds/atom.xml/route.ts
@@ -3,7 +3,7 @@ import generateFeed from "src/utils/generateFeed";
 export async function GET() {
   const feed = await generateFeed();
   return new Response(feed.atom1(), {
-    headers: { "Content-Type": "application/atom+xml" },
+    headers: { "Content-Type": "application/atom+xml; charset=utf-8" },
   });
 }
 
