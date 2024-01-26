fd8ee2d05a018010d5a679a9a3d25982c3c4afd7
Allan Lasser <lasser.allan@gmail.com>
2024-01-26 13:54:07 -0500

    Puts newsletter signup at the bottom of post pages

--- src/app/(site)/posts/[slug]/page.tsx
+++ src/app/(site)/posts/[slug]/page.tsx
@@ -1,14 +1,24 @@
 import { Metadata } from "next";
+import cx from "classnames";
 import Post from "src/components/post";
+import Subscribe from "src/components/subscribe";
 import getPost from "src/data/getPost";
 import { getPublishedPosts } from "src/data/getPosts";
 import layout from "src/styles/layout.module.css";
+import styles from "./page.module.css";
 
 export default async function PostPage({ params }) {
   const post = await getPost(params.slug);
   return (
     <div className={layout.detail}>
       <Post post={post} />
+      <aside className={cx(styles.subscribe, layout.card)}>
+        <header>
+          <h2>Thanks for stopping by!</h2>
+          <p>If you enjoyed this, consider joining my newsletter.</p>
+        </header>
+        <Subscribe />
+      </aside>
     </div>
   );
 }
