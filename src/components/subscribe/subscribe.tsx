0186618a0bad2c20ac8fdc6a3117b84917bccb27
Allan Lasser <lasser.allan@gmail.com>
2024-01-27 14:02:08 -0500

    Mobile styling for subscribe

--- src/components/subscribe/subscribe.tsx
+++ src/components/subscribe/subscribe.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import cx from "classnames";
-import { MailCheckIcon, MailIcon, RssIcon } from "lucide-react";
+import { MailCheckIcon, MailIcon, RssIcon, Send } from "lucide-react";
 import typography from "src/styles/typography.module.css";
 import styles from "./subscribe.module.css";
 import { handleSignup } from "./actions";
@@ -59,16 +59,20 @@ export default function NewsletterSignup(props: NewsletterSignupProps) {
               title='Subscribe via Email'
               disabled={isPending}
             >
-              Subscribe
+              <span className={cx(styles.icon, styles.hideMedium)}>
+                <Send strokeWidth={2} />
+              </span>
             </button>
           </form>
         )}
         <Link
           title='Subscribe via RSS'
           href='/feeds/rss.xml'
-          className={cx(styles.button, styles.icon, styles.rss)}
+          className={cx(styles.button, styles.rss)}
         >
-          <RssIcon strokeWidth={2} />
+          <span className={styles.icon}>
+            <RssIcon strokeWidth={2} />
+          </span>
         </Link>
       </div>
       <p className={typography.finePrint}>
